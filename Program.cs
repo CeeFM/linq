9d481d5f4693795a0f271abc40bd6038b5cc7703
Colin Matteson <seefm@proton.me>
2023-12-10 23:27:25 -0500

    starting linq exercises chapter 15

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Find the words in the collection that start with the letter 'L'
+List<string> fruits = new List<string>() {"Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"};
+
+IEnumerable<string> LFruits = from fruit in fruits
+    where fruit.StartsWith("L")
+    orderby fruit ascending
+    select fruit;
+
+foreach (string c in LFruits)
+{
+    Console.WriteLine($"{c}");
+}
